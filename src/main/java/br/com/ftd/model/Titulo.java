e36953066d545d3577dff38a22ac53a947116ad8
Edson Araujo <edson.caraujo@hotmail.com>
2020-04-22 14:47:21 -0300

    Validações com Bean Validation

--- src/main/java/br/com/ftd/model/Titulo.java
+++ src/main/java/br/com/ftd/model/Titulo.java
@@ -12,6 +12,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
@@ -23,12 +28,19 @@ public class Titulo implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+	
+	@NotEmpty(message = "A descrição é obrigatória")
+	@Size(max = 60, message = "A descrição não pode conter mais de 60 caracteres")
 	private String descricao;
 	
+	@NotNull(message = "A data de vencimento é obrigatória")
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 	
+	@NotNull(message = "O valor não pode ser nulo")
+	@DecimalMin(value = "0.01", message = "O valor não pode ser menor que 0,01")
+	@DecimalMax(value = "9999999.99", message = "O valor não pode ser maio que 9.999.999,99")
 	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
 	
