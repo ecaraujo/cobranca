e46fa9c345c7eb84c1b448138f6609d6d78afdd2
Edson Araujo <edson.caraujo@hotmail.com>
2020-04-26 22:04:24 -0300

    Context path no Tomcat externo

--- src/main/java/br/com/ftd/CobrancaApplication.java
+++ src/main/java/br/com/ftd/CobrancaApplication.java
@@ -5,6 +5,9 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
@@ -17,6 +20,17 @@ public class CobrancaApplication {
 	@Bean
 	public FixedLocaleResolver localResolver() {
 		return new FixedLocaleResolver(new Locale("pt","BR"));
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Configuration
+	public static class MvcConfig extends WebMvcConfigurerAdapter{
+		
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/titulos");
+		}
+		
 	}
 	
 }
